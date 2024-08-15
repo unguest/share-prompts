f00b6031ce7f295ed48f15492c4a3305b468aa4f
unguest <33666394+unguest@users.noreply.github.com>
2024-08-16 01:51:35 +0200

    Adding CodeCov.

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
     email: {
         type: String,
-        unique: [true, 'Email already exists.']},
+        unique: [true, 'Email already exists.'],
         required: [true, 'Email is required.']
     },
     username: {
